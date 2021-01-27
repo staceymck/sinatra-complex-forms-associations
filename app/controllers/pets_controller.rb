d51fe18ac8be25fb1cd8e3c33af29f6b0ba7e9af
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-26 20:37:18 -0600

    commit tree 6cc36621ba2a48bd3491802af5a08931f51f1524

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,23 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @pet = Pet.create(name: params[:pet_name])
 
-    redirect to "pets/#{@pet.id}"
+    if !params[:owner_name].empty?
+      @pet.owner = Owner.create(name: params[:owner_name])
+      @pet.save
+    else
+      @pet.owner = Owner.find_by_id(params[:owner_id])
+      @pet.save
+    end
+
+    redirect to "/pets/#{@pet.id}"
+
   end
 
   get '/pets/:id' do 
@@ -19,7 +30,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @owners = Owner.all 
+    @pet = Pet.find_by_id(params[:id])
+    erb :'/pets/edit'
+  end
+
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params["pet_name"])
+
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+      @pet.save
+    else
+      @pet.owner = Owner.find_by_id(params[:owner_id])
+      @pet.save
+    end
 
     redirect to "pets/#{@pet.id}"
   end
